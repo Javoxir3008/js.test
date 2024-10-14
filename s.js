0f481e2140fb2542af1fe838fecb915e6ca3a1fb
Javoxir3008 <asadbekturakulovic@gmail.com>
2024-10-14 16:34:41 +0500

    Add files via upload

---
+++ s.js
@@ -0,0 +1,42 @@
+// function sayHi(name){
+//     console.log(`hi ${name}`);
+    
+// }
+// sayHi('Alex');
+// sayHi('Allex'); 
+
+
+
+
+
+// const sayHi = function(){
+
+// }
+// sayHi();
+
+
+
+
+// const sayHi = () => {
+//     console.log();
+// }
+// sayHi()
+
+
+
+// Array.map(function () {})
+
+
+// function say(name){
+//     return name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase();
+// }
+// let a = say('alex')
+// let b = say('ALEX')
+
+// console.log(a);
+// console.log(b);
+// document.write(`<h1>${a} ${b}</h1>`)
+
+
+let arr = [0, 1, 1, 2, 3, 5, 8, 12];
+ 
